8faa146d9941dc69227544649525b898f0e1f1d4
CasualAutopsy <Casual_Autopsy@outlook.com>
2026-09-02 15:28:30 -0400

    AdvTCReq v2.2.0

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import TerserPlugin from 'terser-webpack-plugin';
+
+const __dirname = import.meta.dirname ?? path.dirname(fileURLToPath(import.meta.url));
+
+export default {
+    entry: path.join(__dirname, 'src/index.js'),
+    output: {
+        path: path.join(__dirname, 'dist/'),
+        filename: 'index.js',
+    },
+    resolve: {
+        extensions: ['.ts', '.js', '.html'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js/,
+                exclude: /node_modules/,
+                options: {
+                    cacheDirectory: true,
+                    presets: [
+                        '@babel/preset-env',
+                    ],
+                },
+                loader: 'babel-loader',
+            },
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.html$/,
+                use: { loader: 'html-loader' },
+            },
+        ],
+    },
+    optimization: {
+        minimizer: [
+            new TerserPlugin({
+                extractComments: false,
+                terserOptions: {
+                    format: {
+                        comments: false,
+                    },
+                },
+            }),
+        ],
+    },
+};
